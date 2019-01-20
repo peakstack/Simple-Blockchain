03372218513a851e49984aa3bdfde1db1dbe869c
peakstack <43585195+peakstack@users.noreply.github.com>
2019-01-21 00:30:33 +0100

    Create block.cpp

---
+++ block.cpp
@@ -0,0 +1,46 @@
+#include "block.h"
+
+void block::mine(uint_fast64_t difficulty)
+{
+    char *hash_string_c = new char[difficulty + 1];
+
+    uint_fast64_t rounds = 0;
+    while(rounds < difficulty)
+    {
+        hash_string_c[rounds] = '0';
+        ++rounds;
+    }
+    hash_string_c[difficulty] = '\0';
+
+    std::string hash_string(hash_string_c);
+    delete[] hash_string_c;
+
+    do
+    {
+        ++m_nonce;
+        m_hash = calculate_hash();
+    }
+    while (m_hash.substr(0, difficulty) != hash_string);
+    printf("block mined: %s\n", m_hash.c_str());
+}
+
+std::string const block::calculate_hash()
+{
+    std::stringstream string_stream;
+    string_stream << m_index;
+    string_stream << m_previous_block->m_hash;
+    string_stream << m_timestamp;
+    string_stream << m_nonce;
+    Keccak keccak;
+    return keccak(string_stream.str());
+}
+
+block::block(uint_fast64_t index) : m_index(index)
+{
+    m_nonce = 0;
+    m_timestamp = time(nullptr);
+}
+
+void block::update() {
+    m_hash = calculate_hash();
+}
