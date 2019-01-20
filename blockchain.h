a3ff0d47a29829759cdf2d09ce18977625d1dd94
peakstack <43585195+peakstack@users.noreply.github.com>
2019-01-21 00:32:00 +0100

    Create blockchain.h

---
+++ blockchain.h
@@ -0,0 +1,23 @@
+#ifndef BLOCKCHAIN_BLOCKCHAIN_H
+#define BLOCKCHAIN_BLOCKCHAIN_H
+
+
+#include <list>
+#include "block.h"
+
+class blockchain {
+
+public:
+    uint_fast64_t m_difficulty = 1;
+    void add_block(block& target_block);
+    block& get_block(uint_fast64_t index);
+    block& get_last();
+    block& get_genesis();
+    uint_fast64_t size();
+
+private:
+    std::list<block> m_block_chain = std::list<block>();
+};
+
+
+#endif
