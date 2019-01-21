f1c398b31c6140702a5a4494040b8371c11c8e55
peakstack <43585195+peakstack@users.noreply.github.com>
2019-01-21 15:12:34 +0100

    Update blockchain.cpp

--- blockchain.cpp
+++ blockchain.cpp
@@ -10,13 +10,37 @@ block& blockchain::get_block(uint_fast64_t index)
     auto current_pointer = m_block_chain.begin();
     for(auto i = 0; i < index && current_pointer != m_block_chain.end() ; i++ , current_pointer++);
 
-    if(!(current_pointer == m_block_chain.end())) {
+    if(current_pointer != m_block_chain.end())
+    {
         return *current_pointer;
     }
     else
     {
         throw __cpp_lib_uncaught_exceptions;
     }
+}
+
+bool blockchain::is_valid()
+{
+    for (uint_fast64_t i = 0; i < blockchain::size(); ++i)
+    {
+        if(i == 0)
+        {
+            continue;
+        }
+        block& current_block = blockchain::get_block(i);
+        block& previous_block = blockchain::get_block(i - 1);
+
+        if(current_block.m_hash != current_block.calculate_hash())
+        {
+            return false;
+        }
+        else if(previous_block.m_hash != current_block.m_previous_block->m_hash)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 block& blockchain::get_last()
